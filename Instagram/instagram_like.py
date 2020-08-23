fdbf17219f95c81a3c0d1b16ce5dbab85b239b20
choirul84 <aristhu_oracle@yahoo.com>
2020-08-23 12:52:31 +0700

    scraping instagram like + pagination

--- Instagram/instagram_like.py
+++ Instagram/instagram_like.py
@@ -1,27 +1,46 @@
-import requests, json
+import requests, json, time
 
 url = "https://www.instagram.com/graphql/query"
 
-
+end_cursor = ''
 shortcode = input('Please enter shortcode: ')
-variables = {"shortcode":shortcode,"first":50}
-
-params = {
-    'query_hash': 'd5d763b1e2acf209d62d22d184488e57',
-    'variables': json.dumps(variables)
-}
-
-response = requests.get(url, params=params).json()
-
-users = response['data']['shortcode_media']['edge_liked_by']['edges']
-
 count = 0
-for user in users:
 
-    username = user['node']['username']
-    fullname = user['node']['full_name']
-    profile = user['node']['profile_pic_url']
-    count += 1
-    print(username, fullname, profile)
+while 1:
+    variables = {
+        "shortcode":shortcode,
+        "first":50,
+        'after': end_cursor
+    }
 
-print(count)
+    params = {
+        'query_hash': 'd5d763b1e2acf209d62d22d184488e57',
+        'variables': json.dumps(variables),
+
+    }
+
+    response = requests.get(url, params=params).json()
+
+    try:
+        users = response['data']['shortcode_media']['edge_liked_by']['edges']
+    except:
+        print("wait for 30 seconds..")
+        time.sleep(30)
+        continue
+        # continue akan mengembalikan posisi eksekusi script ke variables
+
+    for user in users:
+
+        username = user['node']['username']
+        fullname = user['node']['full_name']
+        profile = user['node']['profile_pic_url']
+        count += 1
+        print(count, username, fullname, profile)
+
+
+    # "after":"QVFBanhPcUZtY0hhQTFIWko5OURuVkd5N0c5NDZuRlR3cjNEOHZvWVdxbS0zMHNXZTVSaVQxdEFIOFNzV2xSRDFXNUNiUHhWV1ZFNTBtZlBPRmg1ZkhYOA=="
+    end_cursor  = response['data']['shortcode_media']['edge_liked_by']['page_info']['end_cursor']
+    # print(end_cursor)
+    has_next = response['data']['shortcode_media']['edge_liked_by']['page_info']['has_next_page']
+    if has_next == False: break
+    time.sleep(2)
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # scraping
 Scraping with python only use request by parsing json api
 1. Scraping Instagram
+    1.1 Instagram like with pagination
