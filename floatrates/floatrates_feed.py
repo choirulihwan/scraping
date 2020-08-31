6e8154a653bc1106a9fcb5e0afe526f0b5de6b70
choirul84 <aristhu_oracle@yahoo.com>
2020-08-31 15:32:34 +0700

    number_format in jinja

--- floatrates/floatrates_feed.py
+++ floatrates/floatrates_feed.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import requests
+import requests, locale
 
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
@@ -17,10 +17,19 @@ def index():
     return render_template('idr_rates.html', datas=json_data.values(), currencies=currencies, selected=data)
 
 
-# global filter
+# global filter tidak menerima argument/parameter
 @app.template_filter()
-def number_format(value):
-    return format(int(value), ',d')
+def number_format_temp(value):
+    locale.setlocale(locale.LC_NUMERIC, '')
+    return locale.format_string("%.*f", (2, value), True)
+
+# jika memerlukan argument/parameter input, gunakan context_processor
+@app.context_processor
+def number_format():
+    def _number_format(value, digit):
+        locale.setlocale(locale.LC_NUMERIC, '')
+        return locale.format_string("%.*f", (digit, value), True)
+    return dict(number_format=_number_format)
 
 
 if __name__ == '__main__':
